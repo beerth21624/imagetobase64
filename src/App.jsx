d8ef82582357631469fa0f247d2094d3b852b48e
beerth21624 <beerzii.4321@gmail.com>
2024-04-05 17:54:05 +0700

    add byte array

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,23 @@ const App = () => {
     alert("Base64 string copied to clipboard!");
   };
 
+  const copyByteArray = () => {
+    navigator.clipboard.writeText(JSON.stringify(base64String.split('').map(c => c.charCodeAt(0))) );
+
+    alert("Base64 string copied to clipboard!");
+  };
+  
+
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      width: '100vw',
-    
-    }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
       <div className="image-uploader">
         <h1>Image to Base64 Converter</h1>
         <div className="upload-container">
@@ -62,6 +70,27 @@ const App = () => {
               cols="50"
               readOnly
               value={base64String}
+              className="output-textarea"
+            />
+          </div>
+        )}
+        {selectedImage && (
+          <div className="preview-container">
+            <img src={selectedImage} alt="Selected" className="preview-image" />
+            <button onClick={copyByteArray} className="copy-button">
+              Copy Byte Array
+            </button>
+          </div>
+        )}
+        {base64String && (
+          <div className="output-container">
+            <textarea
+              rows="10"
+              cols="50"
+              readOnly
+              value={JSON.stringify(
+                base64String.split("").map((c) => c.charCodeAt(0))
+              )}
               className="output-textarea"
             />
           </div>
